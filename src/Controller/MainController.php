aec63cd5f37d1d6613b55b55c1c128169c8ad080
Marcel Bosek <himarseo@gmail.com>
2025-03-04 12:27:34 +0100

    first template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
-    #[Route()]
-    public function homepage(): Response
-    {
-        return new Response('<strong>Starship</strong>: your monopoly...');
-    }
+  #[Route()]
+  public function homepage(): Response
+  {
+
+    $starshipCount = 457;
+
+    $myShip = [
+      'name' => 'ES Sheep',
+      'class' => 'Salvaje',
+      'captain' => 'Pedro Zorro',
+      'status' => 'killing them all',
+    ];
+
+
+    return $this->render('main/homepage.html.twig', [
+      'numberOfStarships' => $starshipCount,
+      'myShip' => $myShip,
+    ]);
+  }
 }
